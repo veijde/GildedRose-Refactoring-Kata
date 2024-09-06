7e39edd23e0dbe53e5ceae59c52aaed4baaf7f1b
VELVVDH <vvdh@velleman.eu>
2024-09-06 15:59:07 +0200

    added backstage passes test and updated normal item test

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -8,12 +8,22 @@ class GildedRoseTest {
 
     @Test
     void testNormalItemUpdateQuality() {
-        Item[] items = new Item[] { new Item("normal item", 0, 0) };
+        Item[] items = new Item[] { new Item("normal item", 5, 2) };
         GildedRose gildedRose = new GildedRose(items);
 
         gildedRose.updateQuality();
 
-        assertEquals(0, gildedRose.items[0].quality);
+        assertEquals(4, gildedRose.items[0].quality);
+        assertEquals(1, gildedRose.items[0].sellIn);
+
+        gildedRose.updateQuality();
+
+        assertEquals(3, gildedRose.items[0].quality);
+        assertEquals(0, gildedRose.items[0].sellIn);
+
+        gildedRose.updateQuality();
+
+        assertEquals(1, gildedRose.items[0].quality);
         assertEquals(-1, gildedRose.items[0].sellIn);
     }
 
@@ -27,6 +37,23 @@ class GildedRoseTest {
         assertEquals(80, app.items[0].quality);
         assertEquals(0, app.items[0].sellIn);
     }
+
+    @Test
+    void testBackstagePasses() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 11, 20) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals(21, app.items[0].quality);
+        assertEquals(10, app.items[0].sellIn);
+
+        app.updateQuality();
+
+        assertEquals(23, app.items[0].quality);
+        assertEquals(9, app.items[0].sellIn);
+    }
+
 
 
 }
