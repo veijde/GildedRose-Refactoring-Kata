4104adaa6ff8deaac41974765a718546cb00b49a
VELVVDH <vvdh@velleman.eu>
2024-09-06 15:52:36 +0200

    added normal item test

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -7,11 +7,18 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class GildedRoseTest {
 
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+    void testGenericItemUpdateQuality() {
+        Item[] items = new Item[] { new Item("normal item", 0, 0) };
+        GildedRose gildedRose = new GildedRose(items);
+
+        // Call the method that updates item quality
+        gildedRose.updateQuality();
+
+        // Assert that after one update, quality is still 0 (it can't be negative)
+        assertEquals(0, gildedRose.items[0].quality);
+
+        // Assert that sellIn decreases by 1
+        assertEquals(-1, gildedRose.items[0].sellIn);
     }
 
 }
