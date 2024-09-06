b29d733e77713ac8d7b84d24865c9e9723146aee
VELVVDH <vvdh@velleman.eu>
2024-09-06 14:28:31 +0200

    added sulfuras

---
+++ Java/src/main/java/com/gildedrose/Sulfuras.java
@@ -0,0 +1,13 @@
+package com.gildedrose;
+
+class Sulfuras extends AbstractItem {
+
+    public Sulfuras(Item item) {
+        super(item);
+    }
+
+    @Override
+    public void updateQuality() {
+        // Sulfuras does not change in quality or sellIn
+    }
+}
